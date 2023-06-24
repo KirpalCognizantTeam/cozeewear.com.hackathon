48fb65c4a8eddab6eb513f50417ee5afeb45d5d9
2130524 <kirpala.venkatesh@cognizant.com>
2023-06-24 11:42:00 +0530

    1st commit

--- src/main/java/com/cozeewear/model/ProductInventory.java
+++ src/main/java/com/cozeewear/model/ProductInventory.java
@@ -1,12 +1,28 @@
 package com.cozeewear.model;
 
+//import annotation for entity and table
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//add annotations for entity and table
+@Entity
+@Table(name="ProductInventory")
 public class ProductInventory {
     //create variables for productInventory class inventoryId, productId, quantity, inventoryDate ,discount
 
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
     private int inventoryId;
     private int productId;
     private int quantity;
     private String inventoryDate;
     private double discount;
 
+    //create default constructor
+    public ProductInventory() {
+        super();
     }
+    //create parameterized constructor
+}
