a8df3a61b847d0188de70c22d253eff2ba600fee
Hema <kirpala.venkatesh@cognizant.com>
2023-06-24 12:30:09 +0530

    4th commit

--- src/main/java/com/cozeewear/controller/ProductController.java
+++ src/main/java/com/cozeewear/controller/ProductController.java
@@ -1,4 +1,5 @@
 package com.cozeewear.controller;
 
 public class ProductController {
+   //
 }
--- src/main/java/com/cozeewear/services/ProductService.java
+++ src/main/java/com/cozeewear/services/ProductService.java
@@ -1,4 +1,9 @@
 package com.cozeewear.services;
 
+import com.cozeewear.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ProductService {
+
+
 }
