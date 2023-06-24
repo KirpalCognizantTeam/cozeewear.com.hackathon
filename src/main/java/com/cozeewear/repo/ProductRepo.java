d6e7b8d4652b0de7ce0dc88c5dcf43f8ec66be0f
2130524 <kirpala.venkatesh@cognizant.com>
2023-06-24 12:34:17 +0530

    Controller service & Repo

--- src/main/java/com/cozeewear/repo/ProductRepo.java
+++ src/main/java/com/cozeewear/repo/ProductRepo.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepo extends JpaRepository<Product, Integer> {
+    //create a method to find product by name
     public List<Product> findByProductName(String productName);
+    //create a method to find product by category
+    public List<Product> findByProductCategory(String productCategory);
 }
