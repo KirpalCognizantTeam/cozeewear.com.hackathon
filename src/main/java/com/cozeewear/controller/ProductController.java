11b5b2a5d9f09c395866bd806e6f1d7bdefc1917
2130524 <kirpala.venkatesh@cognizant.com>
2023-06-24 12:47:54 +0530

    Controller service & Repo

--- src/main/java/com/cozeewear/controller/ProductController.java
+++ src/main/java/com/cozeewear/controller/ProductController.java
@@ -6,7 +6,8 @@ import com.cozeewear.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,22 +20,22 @@ public class ProductController {
 
 
     //To get all products
-    @PostMapping("/products")
+    @GetMapping("/products")
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = productService.getAllProducts();
         return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
     }
 
     //To get products by name
-    @PostMapping("/products/{name")
-    public ResponseEntity<List<Product>> getAllProductsByName(String name) {
+    @GetMapping("/products/{name}")
+    public ResponseEntity<List<Product>> getAllProductsByName(@PathVariable String name) {
         List<Product> products = productService.findByProductName(name);
         return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
     }
 
     //To get products by category
-    @PostMapping("/products/{category")
-    public ResponseEntity<List<Product>> getAllProductsByCategory(String category) {
+    @GetMapping("/products/{category}")
+    public ResponseEntity<List<Product>> getAllProductsByCategory(@PathVariable String category) {
         List<Product> products = productService.findByProductCategory(category);
         return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
     }
