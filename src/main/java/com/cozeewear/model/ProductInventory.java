a39c60a59b02e14fc2be442d1974b0cb84a7c080
Hema <kirpala.venkatesh@cognizant.com>
2023-06-24 11:34:55 +0530

    2nd commit

--- src/main/java/com/cozeewear/model/ProductInventory.java
+++ src/main/java/com/cozeewear/model/ProductInventory.java
@@ -1,12 +1,65 @@
 package com.cozeewear.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+
 public class ProductInventory {
     //create variables for productInventory class inventoryId, productId, quantity, inventoryDate ,discount
 
     private int inventoryId;
     private int productId;
     private int quantity;
+    @CreationTimestamp
+    @Column
     private String inventoryDate;
     private double discount;
 
+    //create a default constructor
+    public ProductInventory() {
+
     }
+    //create a parameterized constructor
+    public ProductInventory(int inventoryId, int productId, int quantity, String inventoryDate, double discount) {
+        this.inventoryId = inventoryId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.inventoryDate = inventoryDate;
+        this.discount = discount;
+    }
+     //create getters and setters for inventoryId, productId, quantity, inventoryDate ,discount
+
+
+    public int getInventoryId() {
+        return inventoryId;
+    }
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+    public int getProductId() {
+        return productId;
+    }
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+    public String getInventoryDate() {
+        return inventoryDate;
+    }
+    public void setInventoryDate(String inventoryDate) {
+        this.inventoryDate = inventoryDate;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
